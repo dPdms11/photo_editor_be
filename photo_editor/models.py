09f2670b8e5fa4d110a757d3abf06d50cc9d3e92
dPdms11 <146039375+dPdms11@users.noreply.github.com>
2024-08-28 17:41:21 -0400

    Add custom user model

--- photo_editor/models.py
+++ photo_editor/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Please provide a valid email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_admin", True)
+        return self.create_user(email, password, **extra_fields)
+    
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager() # Overrides default user manager
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+    
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app 'app_label'?"
+        return True
+    
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
+    
+    @property
+    def username(self):
+        return self.email.split("@")[0]
